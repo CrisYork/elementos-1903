0bff4aa06a2d8420f91d02dba22d4e02ad3624ae
Servicios Especiales <Servicios Especiales@SE06>
2019-06-25 21:39:02 -0500

    ghfhg

--- src/main/java/org/unitec/elementos1903/ControladorProfesor.java
+++ src/main/java/org/unitec/elementos1903/ControladorProfesor.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//ControladorProfesor
 package org.unitec.elementos1903;
 
 import org.springframework.web.bind.annotation.PostMapping;
